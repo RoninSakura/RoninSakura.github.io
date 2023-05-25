2677a32ff757462dce77f7a4f3b0aadb417faa72
Roy <976611019@qq.com>
2023-05-25 02:41:52 -0400

    init

---
+++ cat.js
@@ -0,0 +1,23 @@
+function understand(){
+    document.getElementById('ties').scrollIntoView()
+}
+let check=document.getElementsByClassName("check")[0];
+check.onclick=function(){
+    location.href="./cat.html";
+}
+let home=document.getElementsByClassName("home")[0];
+home.onclick=function(){
+    location.href="./cat-pedigree.html";
+}
+let more_first=document.getElementsByClassName("more_first")[0];
+let information=document.getElementsByClassName("more_information")[0];
+more_first.onclick=function(){
+    if(information.style.display=="block")
+    {
+        information.style.display="none";
+    }
+    else{
+        information.style.display="block";
+    }
+    
+}
---
+++ js.js
@@ -0,0 +1,7 @@
+function understand(){
+    document.getElementById('ties').scrollIntoView()
+}
+let check=document.getElementsByClassName("check")[0];
+check.onclick=function(){
+    location.href="./cat.html";
+}
